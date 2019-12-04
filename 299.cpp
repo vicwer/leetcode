c0fda75868a17310c8fd92c11119e2281ba784a9
KG Duan <876706485@qq.com>
2019-12-04 23:01:46 +0800

    299.cpp 暴力 v2

--- 299.cpp
+++ 299.cpp
@@ -68,3 +68,64 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    string int2string(int val) {
+        if(val == 0)
+        {
+            return "0";
+        }
+        string res = "";
+        while(val > 0)
+        {
+            int tmp = val % 10;
+            val = val / 10;
+            res.insert(res.begin(), tmp + 48);
+        }
+
+        return res;
+    }
+
+    string getHint(string secret, string guess) {
+        int size = secret.size();
+        map<char, int> dict;
+        int countA = 0;
+        int countB = 0;
+
+        for(int i = 0; i < size; i++)
+        {
+            if(secret[i] == guess[i])
+            {
+                secret[i] = 'a';
+                guess[i] = 'a';
+                countA++;
+            }
+            else if(dict.find(secret[i]) == dict.end())
+            {
+                dict[secret[i]] = 1;
+            }
+            else
+            {
+                dict[secret[i]] += 1;
+            }
+        }
+
+        for(int i = 0; i < size; i++)
+        {
+            if(secret[i] == 'a')
+            {
+                continue;
+            }
+            else if(dict.find(guess[i]) != dict.end() && dict[guess[i]] > 0)
+            {
+                countB++;
+                dict[guess[i]] -= 1;
+            }
+        }
+
+        return int2string(countA) + "A" + int2string(countB) + "B";
+
+    }
+};
